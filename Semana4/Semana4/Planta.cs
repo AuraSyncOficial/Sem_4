4bf870c8e37ee4bb69475b5b75c55d2e3762cc87
AuraSyncOficial <120164145+AuraSyncOficial@users.noreply.github.com>
2023-09-23 15:45:39 -0500

    Planta
    Eddi Velásquez Lozano

---
+++ Semana4/Semana4/Planta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana4
+{
+    class Planta
+    {
+        public string Nombre { get; }
+        public int TiempoDeVida { get; private set; }
+        public int FrutosVerdurasPorCosecha { get; }
+        public int ValorSemilla { get; }
+        public int ValorProducto { get; }
+
+        public Planta(string nombre, int tiempoDeVida, int frutosVerdurasPorCosecha, int valorSemilla, int valorProducto)
+        {
+            Nombre = nombre;
+            TiempoDeVida = tiempoDeVida;
+            FrutosVerdurasPorCosecha = frutosVerdurasPorCosecha;
+            ValorSemilla = valorSemilla;
+            ValorProducto = valorProducto;
+        }
+
+        public void Crecer()
+        {
+            TiempoDeVida++;
+        }
+
+        public int Cosechar()
+        {
+            int productosCosechados = FrutosVerdurasPorCosecha;
+            TiempoDeVida = 0;
+            return productosCosechados;
+        }
+
+        public int ValorTotal()
+        {
+            return ValorSemilla + (Cosechar() * ValorProducto);
+        }
+    }
+}
